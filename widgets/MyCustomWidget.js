7550125e6408fa979b90ec73bca9fc01b86b0d77
JacksonWambugu <jaksehsekhja@gmail.com>
2023-06-28 00:03:42 +0300

    commit

---
+++ widgets/MyCustomWidget.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+
+const TicTacToeWidget = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const handleClick = (index) => {
+    if (board[index] || winner) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+
+    const newWinner = calculateWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else {
+      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    }
+  };
+
+  const calculateWinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+
+    return null;
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setCurrentPlayer('X');
+    setWinner(null);
+  };
+
+  const renderCell = (index) => {
+    return (
+      <div className="cell" onClick={() => handleClick(index)}>
+        {board[index]}
+      </div>
+    );
+  };
+
+  return (
+    <div className="tic-tac-toe-widget">
+      <div className="board">
+        {board.map((_, index) => renderCell(index))}
+      </div>
+      <div className="status">
+        {winner ? `Winner: ${winner}` : `Current player: ${currentPlayer}`}
+      </div>
+      <button onClick={resetGame}>Reset</button>
+    </div>
+  );
+};
+
+export default TicTacToeWidget;
